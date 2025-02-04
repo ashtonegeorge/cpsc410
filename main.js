0fa0e952c9398c5ac9d4fccb3d19539cb891a3ca
Ashton George <ashtongeorge17@gmail.com>
2025-02-04 00:56:05 -0500

    tailwind css implementation

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
+const createWindow = () => { // create a basic window for the application to be nested in
   const win = new BrowserWindow({
     width: 800,
     height: 600
   })
 
-  win.loadFile('index.html')
+  win.loadFile('index.html') // load the index file into the window
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(() => { // this runs when the app is initialized, or when it is started for the first time
   createWindow()
 })
