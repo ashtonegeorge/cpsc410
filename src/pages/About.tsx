3c91f2b712fc5af4aad04f64b875619dc71dda6c
caseykuhn05 <caseyksuperstar1@gmail.com>
2025-03-10 10:28:48 -0400

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,18 @@
+import React from 'react'
 export default function About() {
-    return <h2>About</h2>;
+    return (
+        <div>
+            <h2>About</h2>
+            <p>This application was developed by a team of students in Dr. Slonka's CPSC 410 class. The team members are:</p>
+            <ul>
+                <li>Marcus Amerine</li>
+                <li>Ashton George</li>
+                <li>Zachary Krepelka</li>
+                <li>Casey Kuhn</li>
+                <li>Aurembiaix Pifarre Planes</li>
+                <li>Preston Slagle</li>
+                <li>Thomas Urbain</li>
+            </ul>
+        </div>
+    );
 }
