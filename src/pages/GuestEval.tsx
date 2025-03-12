fc12629269f183903ce53a7a03ba4ff3702c7279
caseykuhn05 <caseyksuperstar1@gmail.com>
2025-03-12 09:49:58 -0400

    Update GuestEval.tsx
    I added a button

--- src/pages/GuestEval.tsx
+++ src/pages/GuestEval.tsx
@@ -1,3 +1,10 @@
-export default function GuestEval() {
-    return <h2>Guest Speaker Evaluations</h2>;
+import Button from '../components/Button';
+
+export default function GuestEval({setView}: {setView: React.Dispatch<React.SetStateAction<string>>}) {
+    return (
+    <div> 
+        <h2>Guest Speaker Evaluations</h2>
+    <Button icon="/upload.svg" label='Import Guest Eval File' action={() => Promise.resolve(setView('importGuestEval'))} />
+    </div>
+    );
 }
