d5be8193547f3dd161872d8b21d22f931d911147
Ashton George <ashtongeorge17@gmail.com>
2025-03-11 20:17:20 -0400

    implemented optional icon for buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,8 @@
-export default function Button({label, action} : {label: string; action: () => Promise<void>}) {
+export default function Button({label, icon, action} : {label: string; icon: string | null; action: () => Promise<void>}) {
     return (
-        <button className="bg-(--color-francis-red) w-full p-2 md:p-4 rounded-lg hover:bg-red-700 transition-colors cursor-pointer" onClick={action}>
-            <h1 className="lg:text-xl font-semibold">{label}</h1>
+        <button className="bg-(--color-francis-red) flex justify-center gap-2 align-middle w-full p-2 md:p-4 rounded-lg hover:bg-red-700 transition-colors cursor-pointer" onClick={action}>
+            {icon && <img src={icon} className="w-8 invert" />}
+            <h1 className="lg:text-xl font-semibold my-auto">{label}</h1>
         </button>
     )
 }
