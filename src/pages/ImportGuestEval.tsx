692a63bb62b4193fb06c2ae18d06636e1df91c84
caseykuhn05 <caseyksuperstar1@gmail.com>
2025-03-12 16:57:32 -0400

    Create ImportGuestEval.tsx

---
+++ src/pages/ImportGuestEval.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import Button from '../components/Button';
+
+export default function ImportGuestEval() {
+ 
+return (
+    <div>
+        <div className='pt-12 w-1/2 mx-auto'></div>
+        <h2 className='font-semibold'>Import Guest Speaker File</h2>
+        <p>Upload a CSV file to import guest speaker eval.</p>
+   
+  
+   </div>
+);
+
+}
