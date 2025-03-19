68cdf0656e223dfec1585f21d18292ef8848857a
aurem-pifarre <axp414@francis.edu>
2025-03-19 09:30:56 -0400

    updated academic year page

--- src/pages/AcademicYear.tsx
+++ src/pages/AcademicYear.tsx
@@ -15,23 +15,24 @@ export default function AcademicYear() {
     };
 
     return (
-        <div className="flex flex-col justify-center w-full items-center">
-        <div className='grid grid-cols-2 gap-4'></div>
-            <h1>Add Academic Year & Update Academic Year</h1>
-            
-            <div className="flex flex-col justify-center w-full items-center">
-                <h2 className="text-lg font-semibold mb-2">Add Academic Year</h2>
-                <TextField label="Academic Year" setValue={setNewYear} />
-                <div className='w-1/2'></div>
-                <Button label="Save" action={saveAcademicYear} className="w-full mt-4" />
-            </div>
-            
-            <div className="flex flex-col justify-center w-full items-center">
-                <h2 className="text-lg font-semibold mb-2">Update Academic Year</h2>
-                <TextField label="Academic Year" setValue={setUpdateYear} />
-                <div className='w-1/2'></div>
-                <Button label="Update" action={updateAcademicYear} className="w-full mt-4" />
-            </div>
-        </div>
+            <>
+                <h1>Add Academic Year & Update Academic Year</h1>
+                <div className='grid grid-cols-2 gap-4'>
+                    <div className="flex flex-col justify-center w-full items-center">
+                        <h2 className="text-lg font-semibold mb-2">Add Academic Year</h2>
+                        <TextField label="Academic Year" setValue={setNewYear} />
+                        <div className='w-1/2'></div>
+                        <Button label="Save" action={saveAcademicYear} className="w-full mt-4" />
+                    </div>
+                    
+                    <div className="flex flex-col justify-center w-full items-center">
+                        <h2 className="text-lg font-semibold mb-2">Update Academic Year</h2>
+                        <TextField label="Academic Year" setValue={setUpdateYear} />
+                        <div className='w-1/2'></div>
+                        <Button label="Update" action={updateAcademicYear} className="w-full mt-4" />
+                    </div>
+                </div>
+            </>
+        
     );
 }
