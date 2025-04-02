d5a87d5367f7a733b20c1741fa99110992f30c98
unknown <tc.urbain@gmail.com>
2025-04-02 08:54:35 -0400

    began implementing manual imports for course evaluations; can be used as a template for guest speaker evaluations

--- src/pages/ImportCourseEvalMan.tsx
+++ src/pages/ImportCourseEvalMan.tsx
@@ -1,9 +1,111 @@
-export default function importCourseEvalMan() { 
+import React, { useEffect, useState } from 'react';
+import Button from '../components/Button';
+
+export default function importCourseEvalMan({setView}: {setView: React.Dispatch<React.SetStateAction<string>>}) { 
+    const [success, setSuccess] = useState(false);
+    // the following three state variables are arrays of tuples, where the first element is the id and the second is the name
+    const [courses, setCourses] = useState<[string, string][]>([]);
+    const [semesters, setSemesters] = useState<[string, string][]>([]);
+    const [academicYears, setAcademicYears] = useState<[string, string][]>([]);
+
+    // these three state variables correspond to the dropdowns for course, semester, and academic year
+    const [selectedCourse, setSelectedCourse] = useState('');
+    const [selectedSemester, setSelectedSemester] = useState('');
+    const [selectedAcademicYear, setSelectedAcademicYear] = useState('');
+
+    useEffect(() => {
+        window.ipcRenderer.readCourses().then((result: { id: string, name: string }[]) => {
+            // unfortunately we can't directly reference the state variable, so we have to create a new array
+            const coursesArray = result.map((e) => [e.id, e.name] as [string, string]);
+            setCourses(coursesArray);
+            if (coursesArray.length > 0) { // we set the selected course to the first course in the array by default since that is the first (and automatically selected) course in the dropdown
+                setSelectedCourse(coursesArray[0][0]);
+            }
+        });
+        window.ipcRenderer.readSemesters().then((result: { id: string, name: string }[]) => {
+            const semestersArray = result.map((e) => [e.id, e.name] as [string, string]);
+            setSemesters(semestersArray);
+            if (semestersArray.length > 0) {
+                setSelectedSemester(semestersArray[0][0]);
+            }
+        });
+        window.ipcRenderer.readAcademicYears().then((result: { id: string, name: string }[]) => {
+            const academicYearsArray = result.map((e) => [e.id, e.name] as [string, string]);
+            setAcademicYears(academicYearsArray);
+            if (academicYearsArray.length > 0) {
+                setSelectedAcademicYear(academicYearsArray[0][0]);
+            }
+        });
+    }, [])
+
+    // when the user selects a course, we store the course code in a state variable to be used for uploading
+    const handleSelectedCourseChange = (event: React.ChangeEvent<HTMLSelectElement>) => { setSelectedCourse(event.target.value); };
+    const handleSelectedSemesterChange = (event: React.ChangeEvent<HTMLSelectElement>) => { setSelectedSemester(event.target.value); };
+    const handleSelectedAcademicYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => { setSelectedAcademicYear(event.target.value); };
+
     return (
-        <div>
-            <div className='pt-12 w-1/2 mx-auto'></div>
-            <h2 className='font-semibold'>Import Course Evaluation Data Manually</h2>
-            <p></p>
-        </div>
+        <div className='pt-12 w-1/2 mx-auto'>
+            <h2 className='font-semibold'>Import Course Evaluations Manually</h2>
+            <div>
+                <h1 className='font-semibold'>Select Course</h1>
+                <select onChange={handleSelectedCourseChange} className='text-black bg-white p-2 rounded-lg my-2 w-full'>
+                    {courses.map(([id, name]) => (  
+                        <option className='flex p-1 w-full' value={id} key={id}>
+                            {id}: {name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <h1 className='font-semibold'>Select Semester</h1>
+                <select onChange={handleSelectedSemesterChange} className='text-black bg-white p-2 rounded-lg my-2 w-full'>
+                    {semesters.map(([id, name]) => (  
+                        <option className='flex p-1 w-full' value={id} key={id}>
+                            {name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <h1 className='font-semibold'>Select Academic Year</h1>
+                <select onChange={handleSelectedAcademicYearChange} className='text-black bg-white p-2 rounded-lg my-2 w-full'>
+                    {academicYears.map(([id, name]) => (  
+                        <option className='flex p-1 w-full' value={id} key={id}>
+                            {name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <h1 className='font-semibold'>Select Student</h1>
+
+            </div>
+            <div>
+                <h1 className='font-semibold'>The course Program Goals, Course Learning Outcomes, Instructional Objectives, and Syllabus are an effective guide of the material.</h1>
+                
+            </div>
+            <div>
+                <h1 className='font-semibold'>Overall, I rate this course as excellent.</h1>
+
+            </div>
+            <div>
+                <h1 className='font-semibold'>The course challenged me to improve upon my understanding of the subject.</h1>
+                
+            </div>
+            <div>
+                <h1 className='font-semibold'>The course included instruction in patient evaluation, diagnoses, and management.</h1>
+
+            </div>
+            <div>
+                <h1 className='font-semibold'>After taking the course, I understand the role of the Physician Assistant in regards to providing care to patients with conditions covered in this course.</h1>
+                
+            </div>
+            <div>
+                <h1 className='font-semibold'>The department is always looking for ways to improve the curriculum. What are the reflections regarding the course (for example: course objectives, assessments, labs, etc.)? Please be as specific and provide suggestions, when applicable.</h1>
+
+            </div>
+        <Button icon={null} label="Back" action={() => Promise.resolve(setView('courses'))}/>
+        {success && <p className='text-green-500 font-semibold text-xl'>File uploaded successfully!</p>}
+    </div>
     );
 }
