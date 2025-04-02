c93325024d8e72c981b89a10534821c78142d95a
unknown <tc.urbain@gmail.com>
2025-04-02 09:44:57 -0400

    implemented a dropdown menu to handle survey questions answered with a Likert scale

--- src/pages/ImportCourseEvalMan.tsx
+++ src/pages/ImportCourseEvalMan.tsx
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from 'react';
 import Button from '../components/Button';
 
 export default function importCourseEvalMan({setView}: {setView: React.Dispatch<React.SetStateAction<string>>}) { 
-    const [success, setSuccess] = useState(false);
     // the following three state variables are arrays of tuples, where the first element is the id and the second is the name
     const [courses, setCourses] = useState<[string, string][]>([]);
     const [semesters, setSemesters] = useState<[string, string][]>([]);
     const [academicYears, setAcademicYears] = useState<[string, string][]>([]);
+    const [likertGoals, setLikertGoals] = useState<[string, string][]>([]);
 
     // these three state variables correspond to the dropdowns for course, semester, and academic year
     const [selectedCourse, setSelectedCourse] = useState('');
     const [selectedSemester, setSelectedSemester] = useState('');
     const [selectedAcademicYear, setSelectedAcademicYear] = useState('');
+    const [selectedLikertGoals, setSelectedLikertGoals] = useState('');
 
     useEffect(() => {
         window.ipcRenderer.readCourses().then((result: { id: string, name: string }[]) => {
@@ -42,6 +43,7 @@ export default function importCourseEvalMan({setView}: {setView: React.Dispatch<
     const handleSelectedCourseChange = (event: React.ChangeEvent<HTMLSelectElement>) => { setSelectedCourse(event.target.value); };
     const handleSelectedSemesterChange = (event: React.ChangeEvent<HTMLSelectElement>) => { setSelectedSemester(event.target.value); };
     const handleSelectedAcademicYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => { setSelectedAcademicYear(event.target.value); };
+    const handleSelectedLikertGoals = (event: React.ChangeEvent<HTMLSelectElement>) => { setSelectedLikertGoals(event.target.value); };
 
     return (
         <div className='pt-12 w-1/2 mx-auto'>
@@ -77,12 +79,14 @@ export default function importCourseEvalMan({setView}: {setView: React.Dispatch<
                 </select>
             </div>
             <div>
-                <h1 className='font-semibold'>Select Student</h1>
-
-            </div>
-            <div>
                 <h1 className='font-semibold'>The course Program Goals, Course Learning Outcomes, Instructional Objectives, and Syllabus are an effective guide of the material.</h1>
-                
+                <select onChange={handleSelectedLikertGoals} className='text-black bg-white p-2 rounded-lg my-2 w-full'>
+                    <option className='flex p-1 w-full'>Strongly Agree</option>
+                    <option className='flex p-1 w-full'>Agree</option>
+                    <option className='flex p-1 w-full'>Neutral</option>
+                    <option className='flex p-1 w-full'>Disagree</option>
+                    <option className='flex p-1 w-full'>Strongly Disagree</option>
+                </select>
             </div>
             <div>
                 <h1 className='font-semibold'>Overall, I rate this course as excellent.</h1>
@@ -104,8 +108,7 @@ export default function importCourseEvalMan({setView}: {setView: React.Dispatch<
                 <h1 className='font-semibold'>The department is always looking for ways to improve the curriculum. What are the reflections regarding the course (for example: course objectives, assessments, labs, etc.)? Please be as specific and provide suggestions, when applicable.</h1>
 
             </div>
-        <Button icon={null} label="Back" action={() => Promise.resolve(setView('courses'))}/>
-        {success && <p className='text-green-500 font-semibold text-xl'>File uploaded successfully!</p>}
+        <Button icon={null} label="Back" action={() => Promise.resolve(setView('courseEval'))}/>
     </div>
     );
 }
