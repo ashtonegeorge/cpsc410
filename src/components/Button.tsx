e241f6a7864d0815942ece5430d880a836ec70a6
Ashton George <ashtongeorge17@gmail.com>
2025-04-12 00:01:08 -0400

    add border to button to contrast new bg

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button({label, icon, action} : {label: string; icon: string | null; action: () => Promise<void>}) {
     return (
-        <button className="bg-(--color-francis-red) flex justify-center gap-2 align-middle w-full p-2 md:p-4 rounded-lg hover:bg-red-700 transition-colors cursor-pointer" onClick={action}>
+        <button className="bg-(--color-francis-red) border border-stone-700 flex justify-center gap-2 align-middle w-full p-2 md:p-4 rounded-lg hover:bg-red-700 transition-colors cursor-pointer" onClick={action}>
             {icon && <img src={icon} className="w-8 invert" />}
             <h1 className="lg:text-xl font-semibold my-auto">{label}</h1>
         </button>
