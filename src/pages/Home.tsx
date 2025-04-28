b6d155ab84257c8f5cbd6b761687ee8186eab492
Ashton George <ashtongeorge17@gmail.com>
2025-04-28 11:55:20 -0400

    remove buttons from homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,10 @@ export default function Home({setView}: {setView: React.Dispatch<React.SetStateA
             <div className="flex justify-evenly gap-12 pb-12">
                 <Button label="Course Evaluations" action={() => Promise.resolve(setView('courseEval'))} icon={null}/>
                 <Button label="Guest Speaker Evaluations" action={() => Promise.resolve(setView('guestEval'))} icon={null}/>
+            </div>
+            <div className="flex justify-center gap-12 ">
                 <Button label="Module Grades" action={() => Promise.resolve(setView('grades'))} icon={null}/>
-            </div>
-            <div className="flex justify-evenly gap-12 ">
-                <Button label="Courses" action={() => Promise.resolve(setView('courses'))} icon={null}/>
-                <Button label="Guest Lecturers" action={() => Promise.resolve(setView('guestLecturers'))} icon={null}/>
                 <Button label="Academic Year" action={() => Promise.resolve(setView('academicYear'))} icon={null}/>
-            </div>
-            <div className="flex justify-center w-[30vw] max-w-md mx-auto gap-12 pt-12 ">
-                  <Button label="Manage Questions" action={() => Promise.resolve(setView('questions'))} icon={null}/>
             </div>
         </div>
       </div>
